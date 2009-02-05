8e925f12844b58f64d922a2335962613e80e06ba
Andrei Bocan <zmaxor@gmail.com>
2009-02-05 04:15:19 +0200

    minimalist

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ function goats(flickr_object) {
   console.log(flickr_object);
   var photos = $('#photos');
   $.each(flickr_object.photos.photo, function(index, photo) {
+    if ( index % 50 == 0 ) {
+      photos.append('<img src="http://andascarygoat.com/images/scarygoat-index_03.jpg" width="75" height="75" title="Artistry" />');
+    }
     var url = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_s.jpg";
     photos.append('<img src="' + url + '" title="' + photo.title + '" />');
   });
