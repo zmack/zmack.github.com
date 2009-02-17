4f26a073c19e3ddf4e207cea2db6a84eabc11db7
Andrei Bocan <zmaxor@gmail.com>
2009-02-18 01:22:21 +0200

    Updated. Looks like ass on IE for some strange reason. :3

--- scripts.js
+++ scripts.js
@@ -1,10 +1,35 @@
 function goats(flickr_object) {
+  var urls = {
+    web: "http://andascarygoat.com",
+    twitter: "http://twitter.com/monsieur_pickle",
+    flickr: "http://flickr.com/monsieur_pickle",
+    github: "http://github.com/zmack",
+    lastfm: "http://last.fm/user/zmack",
+    delicious: "http://delicious.com/zmack"
+  }
+
+  function goatImg() {
+    for ( service in urls ) {
+      var div = $('<div class="goat"><a class="' + service +'" href="' + urls[service] + '">&nbsp;</a></div>');
+      delete urls[service];
+      return div;
+    }
+  }
   var photos = $('#photos');
+  var photo;
   $.each(flickr_object.photos.photo, function(index, photo) {
     if ( index % 50 == 0 ) {
-      photos.append('<img src="http://andascarygoat.com/images/scarygoat-index_03.jpg" width="75" height="75" title="Artistry" />');
+      photo = goatImg();
+      photos.append(photo);
     }
     var url = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_s.jpg";
-    photos.append('<img src="' + url + '" title="' + photo.title + '" />');
+    photo = $('<img width="75" height="75" src="' + url + '" title="' + photo.title + '" />');
+
+    photo.hide();
+    photos.append(photo);
+  });
+
+  $('img').load( function() {
+    $(this).fadeIn('slow');
   });
 }
